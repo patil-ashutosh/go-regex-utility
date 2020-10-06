db40cbcb65f83795cf30ed9a5eb3238dd51aab7f
nullawhale <kura98.21@gmail.com>
2020-10-06 16:37:35 +0300

    Validation an IPv4 Address

---
+++ regex/rvalidation/ipv4Validation.go
@@ -0,0 +1,12 @@
+package rvalidation
+
+import "regexp"
+
+// ValidateIPv4 validates a string as an md5 hash.
+func ValidateIPv4(hash string) bool {
+	pattern := `^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$`
+	reg := regexp.MustCompile(pattern)
+	match := reg.MatchString(hash)
+
+	return match
+}
