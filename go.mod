246bac626c091e5f69e2d977b59d14e8eba6a41d
nullawhale <kura98.21@gmail.com>
2020-10-06 16:40:01 +0300

    Fix comment

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/patil-ashutosh/go-regex-utility
 
 go 1.13
+
+require github.com/golangci/golangci-lint v1.31.0 // indirect
--- regex/rvalidation/ipv4Validation.go
+++ regex/rvalidation/ipv4Validation.go
@@ -2,7 +2,7 @@ package rvalidation
 
 import "regexp"
 
-// ValidateIPv4 validates a string as an md5 hash.
+// ValidateIPv4 validates a string as an ipv4 address.
 func ValidateIPv4(hash string) bool {
 	pattern := `^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$`
 	reg := regexp.MustCompile(pattern)
