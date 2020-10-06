00e4fad081d4bcd9ec7854a014744094cef528a5
nullawhale <kura98.21@gmail.com>
2020-10-06 18:43:32 +0300

    Split pattern in sub patterns for ipv4 validation

--- regex/rvalidation/ipv4Validation.go
+++ regex/rvalidation/ipv4Validation.go
@@ -1,10 +1,14 @@
 package rvalidation
 
-import "regexp"
+import (
+	"fmt"
+	"regexp"
+)
 
 // ValidateIPv4 validates a string as an ipv4 address.
 func ValidateIPv4(hash string) bool {
-	pattern := `^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$`
+	class := `([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])`
+	pattern := fmt.Sprintf(`^%[1]s\.%[1]s\.%[1]s\.%[1]s$`, class)
 	reg := regexp.MustCompile(pattern)
 	match := reg.MatchString(hash)
 
