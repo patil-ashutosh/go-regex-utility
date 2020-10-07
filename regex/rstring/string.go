3620923b08d6304a6d957d4f8a2b8a80b66f5b20
patil-ashutosh <ashutosh.s.patil@outlook.com>
2020-10-07 20:06:11 +0530

    Fix Merge Conflicts

--- regex/rstring/string.go
+++ regex/rstring/string.go
@@ -54,7 +54,8 @@ func RemoveNonAlphaNumeric(input string) string {
 	}
 
 	return reg.ReplaceAllString(input, "")
-
+}
+	
 // ContainsSpecialChars checks for special characters.
 func ContainsSpecialChars(s string) bool {
 	isStringAlphabetic := regexp.MustCompile(`^[a-zA-Z0-9]*$`).MatchString
